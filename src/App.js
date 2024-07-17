8c8df1cf0f9ea4d60a5705ee9fe001b9b51f08b8
Seijun2023 <sofiaarturogomez@gmail.com>
2024-07-16 21:39:05 -0500

    aluraflix1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola crisis 3 pero te completare!!
         </p>
         <a
           className="App-link"
